37ad7dc79a80c2ba886efe8bbfa2e013ff856e59
ong-wei-hong <weihong.ong@outlook.com>
2025-01-18 21:38:01 +0800

    add same location error text, esc closes popup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import { Box, Grid2 as Grid, Typography, Button, Card } from '@mui/material';
+import { Box, Grid2 as Grid, Typography, Button, Card, TextField } from '@mui/material';
 import MyAutocomplete from './MyAutocomplete';
 import MyCheckbox from './MyCheckbox';
 import { getLocations, queryPath } from './api/api';
@@ -31,7 +31,8 @@ function App() {
     setShowMap(true);
   }
 
-  const submitReady = !!startLocation && !!endLocation;
+  const submitReady = !!startLocation && !!endLocation && startLocation !== endLocation;
+  const sameLocation = !!startLocation && !!endLocation && startLocation === endLocation;
 
   return (
     <Grid 
@@ -89,9 +90,17 @@ function App() {
                     />
                   </Grid>
                   <Grid size={12} container sx={{justifyContent: 'center'}}>
-                    <Button type="submit" variant="contained" color="primary" disabled={!submitReady}>
-                      Submit
-                    </Button>
+                    <Grid size={12} container sx={{justifyContent: 'center'}}>
+                      <Button type="submit" variant="contained" color="primary" disabled={!submitReady}>
+                        Submit
+                      </Button>
+                    </Grid>
+                    { 
+                      sameLocation &&
+                      <Grid size={12} container sx={{justifyContent: 'center'}}>
+                        <Typography color="error">The same start and end location is selected</Typography>
+                      </Grid>
+                    }
                   </Grid>
                 </Grid>
               </Box>
